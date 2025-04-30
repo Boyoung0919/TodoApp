7b662f83fd6c2207dcb658af74b4fee835257874
Boyoung0919 <choiby0919@gmail.com>
2025-04-30 14:54:02 +0900

    인메모리 데이터베이스 프로그래밍 맛보기

---
+++ TodoApp/TodoApp.Models/ITodoRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace TodoApp.Models
+{
+    public interface ITodoRepository
+    {
+        void Add(Todo model); // input
+        List<Todo> GetAll();  // output
+    }
+}
